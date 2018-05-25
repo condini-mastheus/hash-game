c2688c08ca60b1133f788fd31c5412b22926458c
unknown <matheus.condini@gmail.com>
2018-05-25 09:00:46 -0300

    Fix unused constructor

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import './App.css'
 import Board from './Board'
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props)
-  // }
   render() {
     return (
       <div className="full-container">
